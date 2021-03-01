39264b3d81eff7a5324355dc2ea341992b0bdf0d
Brian Reynolds <brian@brianreynolds.net>
2021-02-28 21:05:51 -0500

    Removed intermediate .zip step when building vmod

--- build.bat
+++ build.bat
@@ -1,15 +1,16 @@
 del .\Paths_of_Glory_%1.vmod
-del .\Paths_of_Glory_%1.zip
 
-7z a Paths_of_Glory_%1.zip *.xml -mx1
-7z a Paths_of_Glory_%1.zip *.html -mx1
-7z a Paths_of_Glory_%1.zip *.vsav -mx1
-7z a Paths_of_Glory_%1.zip moduledata -mx1
-7z a Paths_of_Glory_%1.zip help\ -r -mx1
-7z a Paths_of_Glory_%1.zip images\ -r -mx1
-7z a Paths_of_Glory_%1.zip PathsOfGlory\ -r -mx1
+# a = add to archive
+# -mx1 = fastest compression
+# -tzip = zip archive format
 
-rename Paths_of_Glory_%1.zip Paths_of_Glory_%1.vmod
+7z a Paths_of_Glory_%1.vmod *.xml -mx1 -tzip
+7z a Paths_of_Glory_%1.vmod *.html -mx1 -tzip
+7z a Paths_of_Glory_%1.vmod *.vsav -mx1 -tzip
+7z a Paths_of_Glory_%1.vmod moduledata -mx1 -tzip
+7z a Paths_of_Glory_%1.vmod help\ -r -mx1 -tzip
+7z a Paths_of_Glory_%1.vmod images\ -r -mx1 -tzip
+7z a Paths_of_Glory_%1.vmod PathsOfGlory\ -r -mx1 -tzip
 
 
 
