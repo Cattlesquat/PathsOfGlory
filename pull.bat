dd7f3ae9b22fafd147811df77a3587553f850b16
Brian Reynolds <brian@brianreynolds.net>
2021-12-01 13:06:38 -0500

    10.5 folders in prototypes

---
+++ pull.bat
@@ -0,0 +1,4 @@
+7z x "C:\Users\Brian Reynolds\Documents\Gaming & German Game Rules\Vassal\Paths_of_Glory_%1.vmod" buildfile.xml -aoa
+7z x "C:\Users\Brian Reynolds\Documents\Gaming & German Game Rules\Vassal\Paths_of_Glory_%1.vmod" moduledata -aoa
+
+
